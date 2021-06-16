e41240be7be28c2904252245c485785320b35680
Dzintars Klavins <dzintars.klavins@gmail.com>
2021-06-16 19:05:41 +0300

    feat(base): adds mngmt and dev network outputs

--- terraform/environments/development/000base/outputs.tf
+++ terraform/environments/development/000base/outputs.tf
@@ -3,7 +3,17 @@ output "libvirt_pool" {
   value       = module.libvirt_pool
 }
 
-output "libvirt_network" {
+# output "libvirt_network" {
+#   description = "Libvirt network"
+#   value       = module.libvirt_network
+# }
+
+output "management_network" {
   description = "Libvirt network"
-  value       = module.libvirt_network
+  value       = module.management_network
 }
+
+# output "development_network" {
+#   description = "Libvirt network"
+#   value       = module.development_network
+# }
